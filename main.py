7d73bf15f29870c4f29529a8a64e958aa184d4b9
shendrickx <shendrickx@vangenechten.com>
2025-06-12 15:15:32 +0200

    adding player backbone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+import player
 
 def main():
     pygame.init()
@@ -8,14 +9,21 @@ def main():
     
     BLACK = (0,0,0)
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    p1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
         screen.fill(BLACK)
+        p1.draw(screen)
 
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
