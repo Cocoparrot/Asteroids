5cc15b801058ee28a0a691965049a6f13e7bbe8a
shendrickx <shendrickx@vangenechten.com>
2025-06-20 13:30:03 +0200

    added all player movement and optimisation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-import player
+from player import *
 
 def main():
     pygame.init()
@@ -12,17 +12,27 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    p1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
     
         screen.fill(BLACK)
-        p1.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
+
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
